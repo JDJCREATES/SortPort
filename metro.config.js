e5c73416600907770b527a8ccc063184671c75f5
ctrlAlyDelete <alyssaswinford@yahoo.com>
2025-07-05 16:30:59 -0500

    Fix Metro bundler react-native-svg resolution error

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,7 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
-
-// Add resolver configuration to fix react-native-svg web bundling issue
-config.resolver = {
-  ...config.resolver,
-  alias: {
-    ...config.resolver?.alias,
-    'react-native-svg': path.resolve(__dirname, 'node_modules/react-native-svg/lib/module/web'),
-  },
-};
 
 module.exports = config;
