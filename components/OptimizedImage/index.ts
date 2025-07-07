742eb04932e3f0ccce2551d514b93de75a816068
ctrlAlyDelete <alyssaswinford@yahoo.com>
2025-07-07 17:53:49 -0500

    Fix OptimizedImage platform-specific imports

--- components/OptimizedImage/index.ts
+++ components/OptimizedImage/index.ts
@@ -1,11 +1,11 @@
-  import { Platform } from 'react-native';
+import { Platform } from 'react-native';
 
 let OptimizedImageComponent;
 
 if (Platform.OS === 'web') {
-  OptimizedImageComponent = require('../OptimizedImage.native').OptimizedImage;
+  OptimizedImageComponent = require('./OptimizedImage.web').OptimizedImage;
 } else {
-  OptimizedImageComponent = require('../OptimizedImage.native').OptimizedImage;
+  OptimizedImageComponent = require('./OptimizedImage.native').OptimizedImage;
 }
 
 export const OptimizedImage = OptimizedImageComponent;
